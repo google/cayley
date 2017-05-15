9a303cc3f2bef5b8048af7da8b16bd6c5a743e55
Denys Smirnov <denis.smirnov.91@gmail.com>
2017-05-16 02:13:00 +0300

    shape: and tests for bolt2

--- graph/bolt2/bolt_test.go
+++ graph/bolt2/bolt_test.go
@@ -25,6 +25,7 @@ import (
 	"github.com/cayleygraph/cayley/graph/iterator"
 	"github.com/cayleygraph/cayley/graph/kv"
 	"github.com/cayleygraph/cayley/graph/path/pathtest"
+	"github.com/cayleygraph/cayley/graph/shape/shapetest"
 	"github.com/cayleygraph/cayley/quad"
 )
 
@@ -122,4 +123,8 @@ func TestOptimize(t *testing.T) {
 
 func TestBoltPaths(t *testing.T) {
 	pathtest.RunTestMorphisms(t, makeBolt)
+}
+
+func TestBoltShape(t *testing.T) {
+	shapetest.RunTestShapes(t, makeBolt)
 }
